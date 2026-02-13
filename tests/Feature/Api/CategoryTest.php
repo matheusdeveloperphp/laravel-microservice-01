26a0f87a025bb82ca261c30e864a8387589c8c24
matheusdeveloperphp <matheus.lima@blueservice.com.br>
2026-02-13 16:46:55 -0300

    Error retrieving unique category with fake URL + test to see if it returns a 404 error + test to retrieve unique category.

--- tests/Feature/Api/CategoryTest.php
+++ tests/Feature/Api/CategoryTest.php
@@ -9,6 +9,8 @@ use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    protected $endpoint = '/categories';
+
     /**
      * Get All Categories
      *
@@ -20,7 +22,7 @@ class CategoryTest extends TestCase
         Category::factory()->count(6)->create();
 
         // Acessando a rota de categorias
-        $response = $this->getJson('/categories');
+        $response = $this->getJson("{$this->endpoint}");
 
         // Verificando se o status HTTP é 200 (OK)
         $response->assertOk();
@@ -30,5 +32,34 @@ class CategoryTest extends TestCase
 
         // Verificando se a resposta tem 6 categorias no nó "data"
         $response->assertJsonCount(6, 'data');
+    }
+
+
+    /**
+     * Error gets Single Category
+     * @return void
+     */
+    public function test_error_get_single_category()
+    {
+        $category = 'fake-url';
+
+        // Acessando a rota de categorias
+        $response = $this->getJson("{$this->endpoint}/{$category}");
+        $response->assertStatus(404);
+    }
+
+    /**
+     *  Get Single Category
+     * @return void
+     */
+    public function test_get_single_category()
+    {
+        // Criando uma categoria fake
+        $category = Category::factory()->create();
+        $response = $this->getJson("{$this->endpoint}/{$category->url}");
+
+        //Debug temporário para exibir a resposta no terminal enquanto você está desenvolvendo o teste
+        //$response->dump();
+        $response->assertStatus(200);
     }
 }
